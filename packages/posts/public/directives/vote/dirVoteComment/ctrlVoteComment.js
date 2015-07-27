3211b02ba1a20395d295dfff3577c17a637ba0d6
Milan Laketic <milan.laketic91@gmail.com>
2015-07-27 14:00:04 +0200

    live comment score

--- packages/posts/public/directives/vote/dirVoteComment/ctrlVoteComment.js
+++ packages/posts/public/directives/vote/dirVoteComment/ctrlVoteComment.js
@@ -5,14 +5,13 @@
 angular.module('agronet.posts')
 .controller('ctrlVoteComment',['$scope', 'Post', '$stateParams', '$sce', 'dialogs','$location',
 function($scope, Post, $stateParams, $sce, dialogs,$location){
-	
+
+	$scope.score=0+$scope.comment.scoreUp-$scope.comment.scoreDown;
 	$scope.voteComment = function(value){
-		/*console.log(value);
-		console.log($scope.comment._id);*/
-		/*console.log('post:');
-		console.log($scope.$parent.post);*/
 		$scope.$parent.post.voteComment(value,$scope.comment._id,function(response){
-			console.log(response);
+			$scope.comment.scoreUp=response.scoreUp;
+			$scope.comment.scoreDown=response.scoreDown;
+			$scope.score=0+$scope.comment.scoreUp-$scope.comment.scoreDown;
 		});
 
 	};
