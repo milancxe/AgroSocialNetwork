d912992db53e07cf3a864a9f4b188ab6ad8939c3
Milan Laketic <milan.laketic91@gmail.com>
2015-08-03 14:20:25 +0200

    voting posts final working

--- packages/posts/server/controllers/postUtilsServer.js
+++ packages/posts/server/controllers/postUtilsServer.js
@@ -3,12 +3,13 @@
 
 var mongoose = require('mongoose');
 var PostModel = mongoose.model('PostModel');
+var PostVoteModel = mongoose.model('PostVoteModel');
 //var CommentModel = mongoose.model('CommentModel');
 
 
 	
 
-//var postUtils= require('./postUtilsServer.js');
+var postUtils= require('./postUtilsServer.js');
 
 exports.findAllPostsByCreation=function( userId,lastId,next){
 
@@ -16,10 +17,10 @@ exports.findAllPostsByCreation=function( userId,lastId,next){
 
 	PostModel.find(findCriteria).lean().populate({path:'author',model:'UserModel'})
 		.limit(global.config.paginationSize.posts).sort('-created').exec(function(err,posts){
-			next(posts);
-			/*postUtils.checkUserVotedPost(userId,posts,function (checkedPosts){
+
+			postUtils.checkUserVotedPost(userId,posts,function (checkedPosts){
 				next(checkedPosts);
-			});*/
+			});
 	});
 	
 
@@ -28,24 +29,39 @@ exports.findAllPostsByCreation=function( userId,lastId,next){
 
 exports.checkUserVotedPost = function( userId,posts,next){
 
-	console.log('pozvan sam da proverim:');
-	console.log(userId);
-	console.log(posts);
+
 	if(userId && posts) {
-		/*for(var i=0; i<posts.length;i=i+1){
-			for(var j=0; j<posts[i].scoreUp.length;j=j+1){
-				if(String(userId)===String(posts[i].scoreUp[j])){
-					posts[i].upDownStatus = 1;
-				}
+		//var votedIds={};
+		var counter=0;
+
+		var markVote=function(err,markedOne){
+			counter=counter+1;
+			console.log('iz markiranja:');
+			if(markedOne){
+				var ids=posts.map(function(it) { return it._id; });
+				//trcim i trazim da li postoji lajkovan ako postoji postavim mu status
+	    		for(var ind=0;ind<ids.length;ind=ind+1){
+	    			if(String(markedOne.post)===String(ids[ind])){
+	    				posts[ind].updownStatus=markedOne.voteValue;
+    					break;
+	    			}
+	    		}
+			}else{
+				counter=counter+1;
 			}
-			for(var j=0; j<posts[i].scoreDown.length;j=j+1){
-				if(String(userId)===String(posts[i].scoreDown[j])){
-					posts[i].upDownStatus = 2;
-				}
+			if(posts.length===counter){
+				console.log('saljeM:');
+				console.log(posts);
+				next(posts);
 			}
-		}*/
-		next(posts);
+		};
+		for(var i=0; i<posts.length;i=i+1){
+			PostVoteModel.findOne({author:userId,post:posts[i]},markVote);
+		}
+
+
 	}else{
 		next(posts);
 	}
 };
+
